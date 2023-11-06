5fa89f6f68f12cb11e9ce4863bbbf1db61f034ed
Cyb-GitHub <609356204@qq.com>
2023-11-06 15:46:41 +0800

    测试接口

--- static/config/index.js
+++ static/config/index.js
@@ -5,10 +5,10 @@
   window.SITE_CONFIG = {};
 
   // api接口请求地址
-  //window.SITE_CONFIG['baseUrl'] = 'http://47.92.248.108:8080/renren-fast';
-  //window.SITE_CONFIG['ws'] = 'ws://47.92.248.108:8080/renren-fast';
-  window.SITE_CONFIG['baseUrl'] = 'https://api.lpgcom.net/renren-fast';
-  window.SITE_CONFIG['ws'] = 'wss://api.lpgcom.net/renren-fast';
+  window.SITE_CONFIG['baseUrl'] = 'http://154.38.116.245:8001/renren-fast';
+  window.SITE_CONFIG['ws'] = 'ws://154.38.116.245:8001/renren-fast';
+  // window.SITE_CONFIG['baseUrl'] = 'https://api.lpgcom.net/renren-fast';
+  // window.SITE_CONFIG['ws'] = 'wss://api.lpgcom.net/renren-fast';
    //window.SITE_CONFIG['baseUrl'] = 'http://localhost:9090/renren-fast';
    //window.SITE_CONFIG['ws'] = 'ws://localhost:9090/renren-fast';
 
